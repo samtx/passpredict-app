a6496ce5856976bdb31e5d951bf95cb30ddbf2ed
Sam Friedman <samtx@outlook.com>
2020-11-20 14:55:11 -0600

    Fix passes/{satid} api test by mocking cache and db

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -6,11 +6,17 @@ from app.main import app
 
 client = TestClient(app)
 
-redis_noproc = factories.redis_noproc(host=REDIS_HOST, port=6379)
-redisdb = factories.redisdb('redis_noproc')
+# redis_noproc = factories.redis_noproc(host=REDIS_HOST, port=6379)
+# redisdb = factories.redisdb('redis_noproc')
 
-
-def test_passes_ISS(redisdb):
+def test_passes_ISS(mocker):
+    cache_mock = mocker.patch('app.main.cache', spec=True)  # mock the cache
+    cache_mock.get.return_value = None
+    db_mock = mocker.patch('app.tle.engine')
+    db_mock.connect.return_value.__enter__.return_value.execute.return_value.fetchone.return_value = {
+        'tle1': '1 25544U 98067A   20325.70253472 -.00004237  00000-0 -68774-4 0  9996',
+        'tle2': '2 25544  51.6469 294.0115 0001421  73.7792 273.6742 15.49017875256278'
+    }
     satid = 25544  # Int. Space Station
     params = {
         'lat': 32.1234,
