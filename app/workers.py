ec16ac7fc0e34400d0ed8a8a05b1bb607c0e499f
Sam Friedman <samtx@outlook.com>
2021-09-24 09:34:23 -0500

    Use the host.docker.internal hostname in forwarded_allow_ips for gunicorn setting

--- app/workers.py
+++ app/workers.py
@@ -1,8 +1,15 @@
+import socket
+
 from uvicorn.workers import UvicornWorker as UvicornWorkerBase
+
+docker_host = socket.gethostbyname('host.docker.internal')
+forwarded_allow_ips = list(docker_host) if docker_host else []
+
 
 class UvicornWorker(UvicornWorkerBase):
     CONFIG_KWARGS = {
         "loop": "auto",
         "http": "auto",
         "proxy_headers": True,
+        "forwarded_allow_ips": forwarded_allow_ips,
     }
