90a2cf42d0a9afc4bd827b71d4b17a411067481b
Sam Friedman <samtx@outlook.com>
2021-10-04 12:36:56 -0500

    Add basic status tests for /api /help pages

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,7 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_homepage_status():
     response = client.get("/")
     assert response.status_code == 200
@@ -11,4 +12,14 @@ def test_homepage_status():
 
 def test_about_status():
     response = client.get("/about")
+    assert response.status_code == 200
+
+
+def test_api_home_status():
+    response = client.get("/api")
+    assert response.status_code == 200
+
+
+def test_help_status():
+    response = client.get("/help")
     assert response.status_code == 200
