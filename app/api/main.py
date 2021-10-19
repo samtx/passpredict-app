106e2c5cee3241d7d16a695ac5f19578fee12340
Sam Friedman <samtx@outlook.com>
2021-10-18 20:30:41 -0500

    Cache rendered markdown in /api homepage

--- app/api/main.py
+++ app/api/main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 import markdown
+from starlette.background import BackgroundTasks
 from starlette import middleware
 
 from app import settings
@@ -41,26 +42,37 @@ app.include_router(
 
 
 @app.get('/', include_in_schema=False)
-def home(request: Request):
+async def home(request: Request):
     """
     Render API homepage with explanation on how to use the API
     """
     # Get markdown content
-    fpath = Path(templates.directory) / 'api-home.md'
-    config = {
-        'toc': {
-            'permalink': True,
-            'baselevel': 2,
+    key = "markdown:api-home.md"
+    cache = request.app.state.cache
+    res = await cache.get(key)
+    if not res:
+        # html is not in cache, re-render it
+        fpath = Path(templates.directory) / 'api-home.md'
+        config = {
+            'toc': {
+                'permalink': True,
+                'baselevel': 2,
+            }
         }
-    }
-    with open(fpath, 'r') as f:
-        content = markdown.markdown(
-            f.read(),
-            extensions=['fenced_code', 'toc'],
-            extension_configs=config,
-        )
+        with open(fpath, 'r') as f:
+            content = markdown.markdown(
+                f.read(),
+                extensions=['fenced_code', 'toc'],
+                extension_configs=config,
+            )
+        await cache.set(key, content.encode('utf-8'), ex=86400)  # cache for one day
+    else:
+        content = res.decode('utf-8')
     context = {
         'request': request,
         'content': content,
     }
     return templates.TemplateResponse('api-home.html', context)
+
+
+# def render_markdown()
