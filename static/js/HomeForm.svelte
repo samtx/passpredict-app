c600b2cb7cb6bc0dc31628f217f85b679cac998a
Sam Friedman <samtx@outlook.com>
2022-02-18 17:12:52 -0600

    Call mapbox url directly from client, dont use proxied API

--- static/js/HomeForm.svelte
+++ static/js/HomeForm.svelte
@@ -18,10 +18,36 @@ function resetForm() {
     location = undefined;
 }
 
+
+function getLocationFromFeature(feature){
+    // Parse Mapbox Geocoding API response
+    const name = feature['place_name'];
+    const coords = feature['center'];
+    const lon = coords[0];
+    const lat = coords[1];
+    return {name: name, lon: lon, lat: lat}
+}
+
 async function queryLocationAPI(search_text) {
-    const response = await fetch("api/locations/?" + new URLSearchParams({q: search_text}).toString());
+    const params = {
+        access_token:  "MAPBOX_ACCESS_TOKEN",
+        autocomplete: true,
+        types: [
+            'postcode',
+            'district',
+            'place',
+            'locality',
+            'neighborhood',
+            'address',
+        ],
+    };
+    const base_url = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+    const endpoint = encodeURIComponent(search_text);
+    const response = await fetch(`${base_url}${endpoint}.json?` + new URLSearchParams(params).toString());
     const data = await response.json();
-    return data.locations;
+    const features = data.features;
+    const locations = features.map(getLocationFromFeature);
+    return locations;
 }
 
 function stringToFixedFloat(value, n) {
@@ -63,7 +89,7 @@ $: lonStr = stringToFixedFloat(lon, 4);
             localFiltering={false}
             placeholder="New York, NY"
             inputId="location-search"
-            minCharactersToSearch=1
+            minCharactersToSearch=3
             html5autocomplete={true}
             dataFormType='other'
             lpIgnore={true}
