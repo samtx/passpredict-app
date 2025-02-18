f113763a1b46574d89061c013be1b52d5fe82563
Sam Friedman <samtx@outlook.com>
2025-02-18 17:54:58 -0600

    Add db engine and sessionmaker objects to db.__init__.py file

--- backend-api/api/db/__init__.py
+++ backend-api/api/db/__init__.py
@@ -1,3 +1,6 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from api.settings import config
 from ._base import Base
 from ._models import Satellite
 from ._models import Tle
@@ -9,4 +12,35 @@ __all__ = [
     "Satellite",
     "Tle",
     "Orbit",
+    "ReadSession",
+    "WriteSession",
 ]
+
+
+read_engine = create_async_engine(f"{config.db_url}?immutable=1&mode=ro")
+
+write_engine = create_async_engine(config.db_url)
+
+ReadSession = async_sessionmaker(
+    bind=read_engine,
+    expire_on_commit=False,
+)
+
+WriteSession = async_sessionmaker(
+    bind=write_engine,
+    expire_on_commit=False,
+)
+
+# from sqlalchemy.engine import Engine
+# from sqlalchemy import event, Connection
+# @event.listens_for(read_conn.sync_engine, "connect")
+# def set_sqlite_wal_mode(dbapi_connection, connection_record):
+#     cursor = dbapi_connection.cursor()
+#     cursor.execute("PRAGMA journal_mode=WAL;")
+#     cursor.close()
+
+# @event.listens_for(write_conn.sync_engine, "connect")
+# def set_sqlite_wal_mode(dbapi_connection, connection_record):
+#     cursor = dbapi_connection.cursor()
+#     cursor.execute("PRAGMA journal_mode=WAL;")
+#     cursor.close()
