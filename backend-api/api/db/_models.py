e1b90c7106ab289b6551f658332959af4ad56484
Sam Friedman <samtx@outlook.com>
2025-02-19 21:38:03 -0600

    Comment out FTS table DDL for now

--- backend-api/api/db/_models.py
+++ backend-api/api/db/_models.py
@@ -55,22 +55,22 @@ class CreateFTS5Table(ExecutableDDLElement):
         self.table = table
         self.columns = columns
 
-@compiles(CreateFTS5Table)
-def create_fts5_table(element: CreateFTS5Table, compiler, **kwargs):
-    sql_compiler = compiler.sql_compiler
-    table_name = element.table.__tablename__
-    fts_table_name = f"{table_name}_fts"
-    columns = ", ".join((col for col in element.columns))
-    row_id = sql_compiler.render_literal_value(element.table.primary_key, String())
-    stmt = (
-        f"CREATE VIRTUAL TABLE {fts_table_name} IF NOT EXISTS USING fts5("
-        f"{columns}, content={table_name}, content_rowid={row_id});"
-    )
-    return stmt
+# @compiles(CreateFTS5Table)
+# def create_fts5_table(element: CreateFTS5Table, compiler, **kwargs):
+#     sql_compiler = compiler.sql_compiler
+#     table_name = element.table.__tablename__
+#     fts_table_name = f"{table_name}_fts"
+#     columns = ", ".join((col for col in element.columns))
+#     row_id = sql_compiler.render_literal_value(element.table.primary_key, String())
+#     stmt = (
+#         f"CREATE VIRTUAL TABLE {fts_table_name} IF NOT EXISTS USING fts5("
+#         f"{columns}, content={table_name}, content_rowid={row_id});"
+#     )
+#     return stmt
 
 
 
-listen(Satellite, "after_create", CreateFTS5Table() )
+# listen(Satellite, "after_create", CreateFTS5Table() )
 
 
 class Tle(Base):
